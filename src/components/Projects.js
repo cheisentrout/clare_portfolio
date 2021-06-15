e94063625ca8ac16753b6e1e46b1cf9a1bb51eb3
cheisentrout <cheisentrout@gmail.com>
2021-06-15 11:42:14 -0400

    Changed order of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,21 +12,6 @@ function Projects() {
       <div id="projects-container">
       <h3>projects</h3>
         <ul id="outer-project-ul">
-            <li>
-                <a href="https://pure-meadow-65541.herokuapp.com/home" target="https://pure-meadow-65541.herokuapp.com/home"><img src="../../img/back_pocket.png" alt="back pocket" /></a>
-                <h4>back pocket</h4>
-                <div className="project-icons">
-                    <a href="https://pure-meadow-65541.herokuapp.com/home" target="https://pure-meadow-65541.herokuapp.com/home"><i class="fas fa-external-link-square-alt"></i></a>
-                    <a href="https://github.com/cheisentrout/back-pocket-frontend" target="https://github.com/cheisentrout/back-pocket-frontend"><i class="fab fa-github-square"></i></a>
-                </div>
-                <p>Back Pocket is an app inspired by the Cognitive Behavioral Therapy technique of writing "coping cards", or phrases that help reframe or redirect one's thoughts.</p>
-                <ul id="inner-project-ul">
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Python / Django</li>
-                    <li>Material UI</li>
-                </ul>
-            </li>
             <li id="middle-proj">
                 <a href="http://www.farmarfinder.com/" target="http://www.farmarfinder.com/"><img src="../../img/farmar.png" alt="farmar" /></a>
                 <h4>farmar</h4>
@@ -55,6 +40,21 @@ function Projects() {
                     <li>JavaScript</li>
                     <li>MongoDB</li>
                     <li>Bcrypt</li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://pure-meadow-65541.herokuapp.com/home" target="https://pure-meadow-65541.herokuapp.com/home"><img src="../../img/back_pocket.png" alt="back pocket" /></a>
+                <h4>back pocket</h4>
+                <div className="project-icons">
+                    <a href="https://pure-meadow-65541.herokuapp.com/home" target="https://pure-meadow-65541.herokuapp.com/home"><i class="fas fa-external-link-square-alt"></i></a>
+                    <a href="https://github.com/cheisentrout/back-pocket-frontend" target="https://github.com/cheisentrout/back-pocket-frontend"><i class="fab fa-github-square"></i></a>
+                </div>
+                <p>Back Pocket is an app inspired by the Cognitive Behavioral Therapy technique of writing "coping cards", or phrases that help reframe or redirect one's thoughts.</p>
+                <ul id="inner-project-ul">
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Python / Django</li>
+                    <li>Material UI</li>
                 </ul>
             </li>
         </ul>
